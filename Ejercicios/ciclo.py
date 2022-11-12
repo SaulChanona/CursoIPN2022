04a1a3f9728beb9c3f2f7b30080abecebdba537b
SaulChanona <116772662+SaulChanona@users.noreply.github.com>
2022-11-11 18:38:31 -0600

    Update ciclo.py

--- Ejercicios/ciclo.py
+++ Ejercicios/ciclo.py
@@ -1,16 +1,17 @@
 
 j = float(input("Coloque un numero:"))
 
-if (j < 0 and j > -100):
-    for i in range(-1, -100, -2): 
+if (j < 0 and j >= -100):
+    for j in range(-1, -100, -2): 
         print(f"{j}")
     
-elif (j > 0 and j < 100):
-    for i in range(1, 100, 2): 
+elif (j > 0 and j <= 100):
+    for j in range(1, 100, 2): 
         print(f"{j}")
 
 else: 
     print("Numero ivalido, coloquelo otra vez")
+
 
 
 
