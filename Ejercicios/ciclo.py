a8c261ed46f2205153819c6d14e9ae68ceec5fe5
SaulChanona <116772662+SaulChanona@users.noreply.github.com>
2022-11-11 18:33:13 -0600

    Add files via upload

---
+++ Ejercicios/ciclo.py
@@ -0,0 +1,16 @@
+
+j = float(input("Coloque un numero:"))
+
+if (j < 0 and j > -100):
+    for i in range(-1, -100, -2): 
+        print(f"{j}")
+    
+elif (j > 0 and j < 100):
+    for i in range(1, 100, 2): 
+        print(f"{j}")
+
+else: 
+    print("Numero ivalido, coloquelo otra vez")
+
+
+
---
+++ Ejercicios/ciclos.py
@@ -0,0 +1,22 @@
+ # For() - para
+#i <10
+#for i in range(1, 10, 1): #(Valor inicial valor final)
+  #  print(f"{i}Hola")
+'''
+     Priueba de escritorio
+                variable        proceso
+                    i           imprimir
+                    1           Hola
+                    2           Hola
+                    3           Hola
+                    4           Hola
+                    5           Hola
+                    hasta llegar a 10
+
+
+valor finañ
+    ''' 
+j = 1
+while (j<= 4):
+    print(f"{j}Hola")
+    j = j + 1
